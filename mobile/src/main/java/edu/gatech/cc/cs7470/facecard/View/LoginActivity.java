c98e4f1db0432656310a379a1d9eb8e4e2607278
Mi Seon Park <msp8955@gmail.com>
2015-02-24 17:35:29 -0500

    cleanup

--- mobile/src/main/java/edu/gatech/cc/cs7470/facecard/View/LoginActivity.java
+++ mobile/src/main/java/edu/gatech/cc/cs7470/facecard/View/LoginActivity.java
@@ -1,11 +1,16 @@
 package edu.gatech.cc.cs7470.facecard.View;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.SignInButton;
+
+import edu.gatech.cc.cs7470.facecard.Constants;
 import edu.gatech.cc.cs7470.facecard.R;
 
 /**
@@ -14,20 +19,29 @@ import edu.gatech.cc.cs7470.facecard.R;
 public class LoginActivity extends Activity {
 
     private Activity activity;
-    private Button btnLogin;
+    private SignInButton btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         activity = this;
-        btnLogin = (Button) findViewById(R.id.sign_in_button);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnSignIn = (SignInButton) findViewById(R.id.sign_in_button);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                saveAccountPreference("test_id");
                 Intent i = new Intent(activity, MainActivity.class);
                 startActivity(i);
             }
         });
+    }
+
+    private void saveAccountPreference(String id){
+        SharedPreferences prefs = getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(Constants.SHARED_PREFERENCES_ACCOUNT, id);
+        editor.commit();
     }
 
 
