7f906075d6fc6cb08ad1bfb984132dd7840964f7
Mi Seon Park <msp8955@gmail.com>
2015-02-28 16:09:24 -0500

    changes

--- mobile/src/main/java/edu/gatech/cc/cs7470/facecard/Constants.java
+++ mobile/src/main/java/edu/gatech/cc/cs7470/facecard/Constants.java
@@ -7,6 +7,9 @@ public class Constants {
 
     public static final String PACKAGE_NAME = "edu.gatech.cc.cs7470.facecard";
 
+    //profile layout settings
+    public static final int PROFILE_PIC_SIZE = 400;
+
     //shared preferences labels
     public static final String SHARED_PREFERENCES_ACCOUNT = "account"; //Google+ account
 }
--- mobile/src/main/java/edu/gatech/cc/cs7470/facecard/View/LoginActivity.java
+++ mobile/src/main/java/edu/gatech/cc/cs7470/facecard/View/LoginActivity.java
@@ -7,11 +7,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -20,6 +20,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+import com.google.android.gms.plus.Plus;
+
 
 import edu.gatech.cc.cs7470.facecard.Constants;
 import edu.gatech.cc.cs7470.facecard.R;
@@ -27,8 +29,7 @@ import edu.gatech.cc.cs7470.facecard.R;
 /**
  * Created by miseonpark on 2/24/15.
  */
-public class LoginActivity extends Activity implements
-        ConnectionCallbacks, OnConnectionFailedListener{
+public class LoginActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private static final String TAG = "facecard-login";
 
@@ -82,11 +83,40 @@ public class LoginActivity extends Activity implements
         btnSignIn = (SignInButton) findViewById(R.id.sign_in_button);
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                resolveSignInError();
                 saveAccountPreference("test_id");
                 Intent i = new Intent(activity, MainActivity.class);
                 startActivity(i);
             }
         });
+        mGoogleApiClient = buildGoogleApiClient();
+    }
+
+    private GoogleApiClient buildGoogleApiClient() {
+        // When we build the GoogleApiClient we specify where connected and
+        // connection failed callbacks should be returned, which Google APIs our
+        // app uses and which OAuth 2.0 scopes our app requests.
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     private void saveAccountPreference(String id){
@@ -95,6 +125,81 @@ public class LoginActivity extends Activity implements
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(Constants.SHARED_PREFERENCES_ACCOUNT, id);
         editor.commit();
+    }
+
+    /* Starts an appropriate intent or dialog for user interaction to resolve
+   * the current error preventing the user from being signed in.  This could
+   * be a dialog allowing the user to select an account, an activity allowing
+   * the user to consent to the permissions being requested by your app, a
+   * setting to enable device networking, etc.
+   */
+    private boolean resolveSignInError() {
+        if (mSignInIntent != null) {
+            // We have an intent which will allow our user to sign in or
+            // resolve an error.  For example if the user needs to
+            // select an account to sign in with, or if they need to consent
+            // to the permissions your app is requesting.
+
+            try {
+                // Send the pending intent that we stored on the most recent
+                // OnConnectionFailed callback.  This will allow the user to
+                // resolve the error currently preventing our connection to
+                // Google Play services.
+                mSignInProgress = STATE_IN_PROGRESS;
+                startIntentSenderForResult(mSignInIntent.getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+                return true;
+            } catch (IntentSender.SendIntentException e) {
+                Log.i(TAG, "Sign in intent could not be sent: "
+                        + e.getLocalizedMessage());
+                // The intent was canceled before it was sent.  Attempt to connect to
+                // get an updated ConnectionResult.
+                mSignInProgress = STATE_SIGN_IN;
+                mGoogleApiClient.connect();
+                return false;
+            }
+        } else {
+            // Google Play services wasn't able to provide an intent for some
+            // error types, so we show the default Google Play services error
+            // dialog which may still start an intent on our behalf if the
+            // user can resolve the issue.
+            showDialog(DIALOG_PLAY_SERVICES_ERROR);
+            return true;
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+            case DIALOG_PLAY_SERVICES_ERROR:
+                if (GooglePlayServicesUtil.isUserRecoverableError(mSignInError)) {
+                    return GooglePlayServicesUtil.getErrorDialog(
+                            mSignInError,
+                            this,
+                            RC_SIGN_IN,
+                            new DialogInterface.OnCancelListener() {
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    Log.e(TAG, "Google Play services resolution cancelled");
+                                    mSignInProgress = STATE_DEFAULT;
+                                }
+                            });
+                } else {
+                    return new AlertDialog.Builder(this)
+                            .setMessage(R.string.play_services_error)
+                            .setPositiveButton(R.string.close,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Log.e(TAG, "Google Play services error could not be "
+                                                    + "resolved: " + mSignInError);
+                                            mSignInProgress = STATE_DEFAULT;
+                                        }
+                                    }).create();
+                }
+            default:
+                return super.onCreateDialog(id);
+        }
     }
 
     @Override
@@ -111,38 +216,4 @@ public class LoginActivity extends Activity implements
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
-
-//    @Override
-//    protected Dialog onCreateDialog(int id) {
-//        switch(id) {
-//            case DIALOG_PLAY_SERVICES_ERROR:
-//                if (GooglePlayServicesUtil.isUserRecoverableError(mSignInError)) {
-//                    return GooglePlayServicesUtil.getErrorDialog(
-//                            mSignInError,
-//                            this,
-//                            RC_SIGN_IN,
-//                            new DialogInterface.OnCancelListener() {
-//                                @Override
-//                                public void onCancel(DialogInterface dialog) {
-//                                    Log.e(TAG, "Google Play services resolution cancelled");
-//                                    mSignInProgress = STATE_DEFAULT;
-//                                }
-//                            });
-//                } else {
-//                    return new AlertDialog.Builder(this)
-//                            .setMessage(R.string.play_services_error)
-//                            .setPositiveButton(R.string.close,
-//                                    new DialogInterface.OnClickListener() {
-//                                        @Override
-//                                        public void onClick(DialogInterface dialog, int which) {
-//                                            Log.e(TAG, "Google Play services error could not be "
-//                                                    + "resolved: " + mSignInError);
-//                                            mSignInProgress = STATE_DEFAULT;
-//                                        }
-//                                    }).create();
-//                }
-//            default:
-//                return super.onCreateDialog(id);
-//        }
-//    }
 }
