1c77b2addfbab667ac4ec0ab7e6e5fd157e64ff1
Mi Seon Park <msp8955@gmail.com>
2015-04-15 13:58:34 -0400

    add interface

---
+++ mobile/src/main/java/edu/gatech/cc/cs7470/facecard/Controller/services/OnTaskCompleted.java
@@ -0,0 +1,10 @@
+package edu.gatech.cc.cs7470.facecard.Controller.services;
+
+import edu.gatech.cc.cs7470.facecard.Model.FaceCard;
+
+/**
+ * Created by miseonpark on 4/14/15.
+ */
+public interface OnTaskCompleted {
+    void onTaskCompleted(FaceCard[] cards);
+}
